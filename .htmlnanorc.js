213e69e27331949c26a42b6ef5bdd7528f983477
nicheweb <norgerojascerulia@gmail.com>
2022-07-07 13:05:18 +0200

    Responsive

--- .htmlnanorc.js
+++ .htmlnanorc.js
@@ -1,3 +1,4 @@
+// file added due to the bug: /opt/build/repo/src/index.html: tree.render is not a function(seem to appear due to parcel trying to minify SVG)
 module.exports = {
   minifySvg: false,
 };
